1ed1df1adfc3005f6783d330aaea91faa99afc14
Thomas Elder <tgelder@gmail.com>
2017-10-31 19:41:31 +0000

    SeaLevel fun

--- core/src/com/tgelder/TerrainViewer.java
+++ core/src/com/tgelder/TerrainViewer.java
@@ -2,6 +2,7 @@ package com.tgelder;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,77 +16,97 @@ import com.tgelder.downhill.renderer.HeightRenderer;
 import com.tgelder.downhill.terrain.Terrain;
 import com.tgelder.image.PixmapImage;
 
-import java.util.Optional;
-
 public class TerrainViewer extends ApplicationAdapter {
-	private SpriteBatch batch;
-    private SpriteBatch batch2;
-	private Sprite sprite;
-    private BitmapFont font;
-	private OrthographicCamera cam;
-	private TerrainInfo terrainInfo;
+  private Terrain terrain;
+  private SpriteBatch batch;
+  private SpriteBatch batch2;
+  private Sprite sprite = new Sprite();
+  private BitmapFont font;
+  private OrthographicCamera cam;
+  private TerrainInfo terrainInfo;
+  private int seaLevel = 0;
 
-	@Override
-	public void create () {
+  @Override
+  public void create () {
 
-		batch = new SpriteBatch();
-		batch2 = new SpriteBatch();
+    batch = new SpriteBatch();
+    batch2 = new SpriteBatch();
 
 
-		Terrain terrain = new Terrain(1986, 10, 3000);
-		PixmapImage image = new PixmapImage(1024, 1024);
-		HeightRenderer renderer = new HeightRenderer();
-		renderer.render(terrain.getAltitudes(), 0, 3000, image);
+    terrain = new Terrain(1986, 10, 3000);
 
-		Texture texture = new Texture(image.getPixmap());
-		sprite = new Sprite(texture);
+    updateSprite();
 
-		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
-		cam.update();
+    cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+    cam.update();
 
-		terrainInfo = new TerrainInfo(terrain, cam);
+    terrainInfo = new TerrainInfo(terrain, cam);
 
-		InputMultiplexer multiplexer = new InputMultiplexer();
-		multiplexer.addProcessor(new GestureDetector(new OrphographicCameraController(cam)));
-		multiplexer.addProcessor(terrainInfo);
+    InputMultiplexer multiplexer = new InputMultiplexer();
+    multiplexer.addProcessor(new GestureDetector(new OrphographicCameraController(cam)));
+    multiplexer.addProcessor(terrainInfo);
 
-		Gdx.input.setInputProcessor(multiplexer);
+    Gdx.input.setInputProcessor(multiplexer);
 
-		font = new BitmapFont();
-		font.setColor(Color.RED);
-	}
+    font = new BitmapFont();
+    font.setColor(Color.RED);
+  }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+  @Override
+  public void render () {
 
-		batch.setProjectionMatrix(cam.combined);
+    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+      seaLevel = Math.min(seaLevel + 5, 3000);
+      updateSprite();
+    }
+    if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+      seaLevel = Math.max(seaLevel - 5, 0);
+      updateSprite();
+    }
 
-		batch.begin();
-		sprite.setPosition(0, 0);
-		sprite.draw(batch);
-		batch.end();
+    Gdx.gl.glClearColor(1, 1, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch2.begin();
-		font.draw(batch2, terrainInfo.getAltitude() + "\n" + terrainInfo.getX() + "\n" + terrainInfo.getY()
-        , 200, 200);
-		batch2.end();
+    batch.setProjectionMatrix(cam.combined);
 
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+    batch.begin();
+    sprite.setPosition(0, 0);
+    sprite.draw(batch);
+    batch.end();
 
-	@Override
-	public void resize(int width, int height) {
-		cam.viewportWidth = Gdx.graphics.getWidth();
-		cam.viewportHeight = Gdx.graphics.getHeight();
-		cam.update();
-	}
+    batch2.begin();
+    font.draw(batch2, terrainInfo.getAltitude() + "\n" + terrainInfo.getX() + "\n" + terrainInfo.getY()
+            , 200, 200);
+    batch2.end();
+
+  }
+
+  @Override
+  public void dispose () {
+    batch.dispose();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+    cam.viewportWidth = Gdx.graphics.getWidth();
+    cam.viewportHeight = Gdx.graphics.getHeight();
+    cam.update();
+  }
+
+
+  private void updateSprite() {
+
+    PixmapImage image = new PixmapImage(1024, 1024);
+
+
+
+    HeightRenderer renderer = new HeightRenderer();
+    renderer.render(terrain.getAltitudes(), seaLevel, 3000, image);
+
+    Texture texture = new Texture(image.getPixmap());
+    sprite = new Sprite(texture);
+  }
 
 }
