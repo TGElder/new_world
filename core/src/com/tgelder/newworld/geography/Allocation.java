260a5cb60083603edd7a175a1c61919baf27b34e
Thomas Elder <tgelder@gmail.com>
2017-11-14 21:18:44 +0000

    Finished (and simplified) Allocation method, added 1st test

--- core/src/com/tgelder/newworld/geography/Allocation.java
+++ core/src/com/tgelder/newworld/geography/Allocation.java
@@ -6,61 +6,60 @@ import com.tgelder.network.Network;
 import com.tgelder.network.NetworkSearch;
 import lombok.Getter;
 
+import java.util.Map;
 import java.util.Set;
 
 @Getter
 public class Allocation {
 
-  private final ImmutableList<Resource> metDemand;
-  private final ImmutableList<Resource> metSupply;
-  private final ImmutableList<Resource> unmetDemand;
-  private final ImmutableList<Resource> unmetSupply;
+  private final ImmutableList<Integer> metDemand;
+  private final ImmutableList<Integer> metSupply;
+  private final ImmutableList<Integer> unmetDemand;
+  private final ImmutableList<Integer> unmetSupply;
 
-  public Allocation(ResourceType resourceType, ImmutableList<Settlement> settlements, Network<Integer> network) {
+  public Allocation(ImmutableList<Integer> demand,
+                    Map<Integer, Integer> supply,
+                    Network<Integer> network) {
 
-    ImmutableList.Builder<Resource> metDemandBuilder = ImmutableList.builder();
-    ImmutableList.Builder<Resource> metSupplyBuilder = ImmutableList.builder();
-    ImmutableList.Builder<Resource> unmetDemandBuilder = ImmutableList.builder();
-    ImmutableList.Builder<Resource> unmetSupplyBuilder = ImmutableList.builder();
+    ImmutableList.Builder<Integer> metDemandBuilder = ImmutableList.builder();
+    ImmutableList.Builder<Integer> metSupplyBuilder = ImmutableList.builder();
+    ImmutableList.Builder<Integer> unmetDemandBuilder = ImmutableList.builder();
+    ImmutableList.Builder<Integer> unmetSupplyBuilder = ImmutableList.builder();
 
-    int[] demandUnmet = new int[settlements.size()];
-    int totalDemandUnmet = 0;
+    int[] free = new int[network.getNodes().size()];
+    int totalFree = 0;
 
-    for (Settlement settlement : settlements) {
-      int demandAtSettlement = settlement.getDemand().get(resourceType).size();
-      demandUnmet[settlement.getIndex()] = demandAtSettlement;
-      totalDemandUnmet += demandAtSettlement;
+
+    for (Map.Entry<Integer, Integer> entry : supply.entrySet()) {
+      int supplyAtSettlement = entry.getValue();
+      free[entry.getKey()] = supplyAtSettlement;
+      totalFree += supplyAtSettlement;
     }
 
-    for (Settlement settlement : settlements) {
+    for (Integer d : demand) {
 
-      for (Resource supply : settlement.getSupply().get(resourceType)) {
-        if (totalDemandUnmet == 0) {
-          unmetSupplyBuilder.add(supply);
+      if (totalFree == 0) {
+        unmetDemandBuilder.add(d);
+      } else {
+        Set<Integer> candidates = NetworkSearch.search(network, d,
+                                                       new FindClosest<>(d, (n) -> free[n] > 0));
+
+        if (candidates.isEmpty()) { //TODO test for this case
+          unmetDemandBuilder.add(d);
         } else {
-
-          Set<Integer> demandSettlements = NetworkSearch.search(network, settlement.getIndex(),
-                  new FindClosest<>(settlement.getIndex(), (n) -> demandUnmet[settlement.getIndex()] > 0));
-
-          if (demandSettlements.isEmpty()) { //TODO test for this case
-            unmetSupplyBuilder.add(supply);
-          } else {
-            int demandSettlement = demandSettlements.stream().findFirst().get();
-            Resource demand = settlements.get(demandSettlement).getDemand().get(resourceType).get(demandUnmet[demandSettlement] - 1);
-            metSupplyBuilder.add(supply);
-            metDemandBuilder.add(demand);
-            demandUnmet[demandSettlement]--;
-          }
-
+          int choiceIndex = candidates.stream().findFirst().get();
+          metDemandBuilder.add(d);
+          metSupplyBuilder.add(choiceIndex);
+          free[choiceIndex]--;
         }
+
       }
     }
 
-    int d;
-    for (Settlement settlement : settlements) {
-      while ((d = demandUnmet[settlement.getIndex()]) > 0) {
-        unmetDemandBuilder.add(settlements.get(d).getDemand().get(resourceType).get(demandUnmet[d] - 1));
-        demandUnmet[settlement.getIndex()]--;
+    for (int s = 0; s < supply.size(); s++) {
+      while (free[s] > 0) {
+        unmetSupplyBuilder.add(s);
+        free[s]--;
       }
     }
 
@@ -68,24 +67,6 @@ public class Allocation {
     metSupply = metSupplyBuilder.build();
     unmetDemand = unmetDemandBuilder.build();
     unmetSupply = unmetSupplyBuilder.build();
-  }
-
-  private boolean unmetDemandAt(boolean demandMet[][], int settlement) {
-    for (int d = 0; d < demandMet.length; d++) {
-      if (!demandMet[settlement][d]) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private int indexOfUnmetDemandAt(boolean demandMet[][], int settlement) {
-    for (int d = 0; d < demandMet.length; d++) {
-      if (!demandMet[settlement][d]) {
-        return d;
-      }
-    }
-    return -1;
   }
 
 }
--- core/test/com/tgelder/newworld/geography/AllocationTest.java
+++ core/test/com/tgelder/newworld/geography/AllocationTest.java
@@ -1,4 +1,30 @@
 package com.tgelder.newworld.geography;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.tgelder.network.Network;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AllocationTest {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void shouldAllocateClosestSupply() {
+    int[] neighbourDXs = {-1, 0, 1, 0};
+    int[] neighbourDYs = {0, -1, 0, 1};
+
+    Network network = Network.createGridNetwork(10, 1, neighbourDXs, neighbourDYs);
+
+    Allocation allocation = new Allocation(ImmutableList.of(5),
+                                           ImmutableMap.of(1, 1, 6, 1),
+                                           network);
+
+    assertThat(allocation.getMetDemand()).containsExactly(5);
+    assertThat(allocation.getMetSupply()).containsExactly(6);
+    assertThat(allocation.getUnmetDemand()).isEmpty();
+    assertThat(allocation.getUnmetSupply()).containsExactly(1);
+
+  }
 }
